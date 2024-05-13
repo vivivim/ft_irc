8f2299119c59c1957fa0be4d9cb18d93e793df3c
vivivim <130360809+vivivim@users.noreply.github.com>
2024-05-13 20:47:07 +0900

    kqueue echo server.cpp

--- echo_server.cpp
+++ echo_server.cpp
@@ -5,8 +5,32 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+#include <sys/types.h>
+#include <sys/event.h>
+#include <sys/time.h>
+#include <fcntl.h>
+
+#include <map>
+#include <vector>
+
+
 const int PORT = 8080;
 const int BUFFER_SIZE = 1024;
+
+void	changeEvents(std::vector<struct kevent>& changeList, uintptr_t ident, int16_t filter, uint16_t flags, uint32_t fflags, intptr_t data, void *udata)
+{
+	struct kevent	tempEvent;
+
+	EV_SET(&tempEvent, ident, filter, flags, fflags, data, udata);
+	changeList.push_back(tempEvent);
+}
+
+void	disconnect_client(int client_fd, std::map<int, std::string> &clients)
+{
+	std::cout << "client disconnected: " << client_fd << std::endl;
+	close(client_fd);
+	clients.erase(client_fd);
+}
 
 int main() {
     // 소켓 생성
@@ -36,38 +60,91 @@ int main() {
 
     std::cout << "Echo server is listening on port " << PORT << std::endl;
 
-    // 클라이언트 요청 처리
-    int client_socket;
-    sockaddr_in client_address;
-    socklen_t client_address_size = sizeof(client_address);
+    int	kq = kqueue();
+	if (kq == -1)
+		kqueue() error;
 
-    while (true) {
-        // 클라이언트 연결 수락
-        client_socket = accept(server_socket, (struct sockaddr *)&client_address, &client_address_size);
-        if (client_socket < 0) {
-            std::cerr << "Error: Accept failed\n";
-            return EXIT_FAILURE;
-        }
+	std::map<int, std::string>	clients;
+	std::vector<struct kevent>	changeList;
+	struct kevent				eventList[8];
 
-        // 클라이언트로부터 메시지 수신 및 에코
-        char buffer[BUFFER_SIZE] = {0};
-        int valread = read(client_socket, buffer, BUFFER_SIZE);
-        if (valread < 0) {
-            std::cerr << "Error: Read failed\n";
-            return EXIT_FAILURE;
-        }
+	changeEvents(changeList, server_socket, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, NULL);
+	std::cout << "echo server with kqueue started '-'" << std::endl;
+	
+	int	newEvents;
+	struct kevent	*currEvent;
+	
+	while (1)
+	{
+		newEvents = kevent(kq, &changeList[0], changeList.size(), eventList, 8, NULL);
+		if (newEvents == -1)
+			kevent error;
+		changeList.clear();
 
-        std::cout << "Received message from client: " << buffer << std::endl;
+		for (int i = 0; i < newEvents; ++i)
+		{
+			currEvent = &eventList[i];
 
-        // 클라이언트에게 메시지 에코
-        if (send(client_socket, buffer, strlen(buffer), 0) < 0) {
-            std::cerr << "Error: Send failed\n";
-            return EXIT_FAILURE;
-        }
+			if (currEvent->flags & EV_ERROR)
+			{
+				error("currEvent->ident socket has problem o0o;;");
+				if (currEvent->ident != server_socket)
+					disconnect_client(currEvent->ident, clients);
+			}
+			else if (currEvent->filter == EVFILT_READ)
+			{
+				if (currEvent->ident == server_socket)
+				{
+					welcome new client();
+				}
+				else if (clients.find(currEvent->ident) != clients.end())
+				{
+					char	buf[1024];
+					int		n = read(currEvent->ident, buf, sizeof(buf));
 
-        // 클라이언트 소켓 닫기
-        close(client_socket);
-    }
+					if (n <= 0)
+					{
+						if (n < 0)
+							error("currEvent->ident client socket has problem o0o;;");
+						disconnect_client(currEvent->ident, clients);
+					}
+					else
+					{
+						buf[n] = '\0';
+						client[currEvent->ident] += buf;
+					}
+				}
+			}
+			else if (currEvent->filter == EVFILT_WRITE)
+			{
+				std::map<int , std::string>::iterator it = clients.find(currEvent->ident);
+
+				if (it != clients.end())
+				{
+					if (clients[currEvent->ident] != "")
+					{
+						int n = write(currEvent->ident, clients[currEvent->ident].c_str(), clients[currEvent->ident].size());
+						if (n < 0)
+						{
+							error write
+							disconnect_client(currEvent->ident, clients);
+						}
+						else
+						{
+							clients[currEvent->ident].clear();
+						}
+					}
+				}
+			}
+		}
+
+	}
+
+    // 클라이언트 소켓 닫기
+	std::map<int, std::string>::iterator	it = clients.begin();
+	for (; it != clients.end(); ++it)
+		close(it->first);
+	clients.clear();
 
     // 서버 소켓 닫기
     close(server_socket);
---
+++ kqueueServer.cpp
@@ -0,0 +1,161 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/event.h>
+#include <sys/time.h>
+#include <fcntl.h>
+
+#include <map>
+#include <vector>
+
+const int PORT = 8080;
+const int BUFFER_SIZE = 1024;
+
+void ChangeEvents(std::vector<struct kevent>& changeList, uintptr_t ident, int16_t filter, uint16_t flags, uint32_t fflags, intptr_t data, void *udata)
+{
+    struct kevent tempEvent;
+    EV_SET(&tempEvent, ident, filter, flags, fflags, data, udata);
+    changeList.push_back(tempEvent);
+}
+
+void disconnectClient(int clientFd, std::map<int, std::string> &clients)
+{
+    std::cout << "Client disconnected: " << clientFd << std::endl;
+    close(clientFd);
+    clients.erase(clientFd);
+}
+
+int main()
+{
+    // Create socket
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == -1)
+    {
+        std::cerr << "Error: Could not create socket\n";
+        return EXIT_FAILURE;
+    }
+
+    // Set socket address
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(PORT);
+
+    // Bind socket
+    if (bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0)
+    {
+        std::cerr << "Error: Bind failed\n";
+        return EXIT_FAILURE;
+    }
+
+    // Listen for client connections
+    if (listen(serverSocket, 5) < 0)
+    {
+        std::cerr << "Error: Listen failed\n";
+        return EXIT_FAILURE;
+    }
+
+    std::cout << "Echo server is listening on port " << PORT << std::endl;
+
+    int kq = kqueue();
+    if (kq == -1)
+    {
+        std::cerr << "Error: kqueue() failed\n";
+        return EXIT_FAILURE;
+    }
+
+    std::map<int, std::string> clients;
+    std::vector<struct kevent> changeList;
+    struct kevent eventList[8];
+
+    ChangeEvents(changeList, serverSocket, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, NULL);
+    std::cout << "Echo server with kqueue started '-'" << std::endl;
+
+    int newEvents;
+    struct kevent *currEvent;
+
+    while (1)
+    {
+        newEvents = kevent(kq, &changeList[0], changeList.size(), eventList, 8, NULL);
+        if (newEvents == -1)
+        {
+            std::cerr << "Error: kevent() failed\n";
+            return EXIT_FAILURE;
+        }
+        changeList.clear();
+
+        for (int i = 0; i < newEvents; ++i)
+        {
+            currEvent = &eventList[i];
+
+            if (currEvent->flags & EV_ERROR)
+            {
+                std::cerr << "Error: Event error occurred\n";
+                if (currEvent->ident != serverSocket)
+                    disconnectClient(currEvent->ident, clients);
+            }
+            else if (currEvent->filter == EVFILT_READ)
+            {
+                if (currEvent->ident == serverSocket)
+                {
+                    std::cout << "Welcome new client\n";
+                }
+                else if (clients.find(currEvent->ident) != clients.end())
+                {
+                    char buf[BUFFER_SIZE];
+                    int n = recv(currEvent->ident, buf, sizeof(buf), 0);
+
+                    if (n <= 0)
+                    {
+                        if (n < 0)
+                            std::cerr << "Error: Client socket has problem\n";
+                        disconnectClient(currEvent->ident, clients);
+                    }
+                    else
+                    {
+                        buf[n] = '\0';
+                        clients[currEvent->ident] += buf;
+                    }
+                }
+            }
+            else if (currEvent->filter == EVFILT_WRITE)
+            {
+                std::map<int , std::string>::iterator it = clients.find(currEvent->ident);
+
+                if (it != clients.end())
+                {
+                    if (!clients[currEvent->ident].empty())
+                    {
+                        int n = send(currEvent->ident, clients[currEvent->ident].c_str(), clients[currEvent->ident].size(), 0);
+                        if (n < 0)
+                        {
+                            std::cerr << "Error: Write failed\n";
+                            disconnectClient(currEvent->ident, clients);
+                        }
+                        else
+                        {
+                            clients[currEvent->ident].clear();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // Close client sockets
+    std::map<int, std::string>::iterator it = clients.begin();
+    for (; it != clients.end(); ++it)
+        close(it->first);
+    clients.clear();
+
+    // Close server socket
+    close(serverSocket);
+
+    return EXIT_SUCCESS;
+}
+
