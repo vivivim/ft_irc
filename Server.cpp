9f38d3a7c3945ba69b292d881399fda9cb672e84
vivivim <130360809+vivivim@users.noreply.github.com>
2024-05-19 21:42:17 +0900

    function refactoring

--- Server.cpp
+++ Server.cpp
@@ -47,6 +47,7 @@ void	Server::create()
 	this->kq = kqueue();
 	if (this->kq == -1)
 		throw	std::runtime_error("kqueue() failed");
+	changeEvents(changeList, socket.getSocket(), EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, NULL);
 }
 
 void Server::changeEvents(std::vector<struct kevent>& changeList, uintptr_t ident, int16_t filter, uint16_t flags, uint32_t fflags, intptr_t data, void *udata)
@@ -58,87 +59,86 @@ void Server::changeEvents(std::vector<struct kevent>& changeList, uintptr_t iden
 
 void	Server::run()
 {
-	this->kq = kqueue();
-	if (this->kq == -1)
-		throw	std::runtime_error("kqueue() failed");
-	changeEvents(changeList, socket.getSocket(), EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, NULL);
-
 	int newEvents;
 	struct kevent *currEvent;
 	while (1)
 	{
 		newEvents = kevent(kq, &changeList[0], changeList.size(), eventList, 8, NULL);
 		if (newEvents == -1)
-		{
 			throw	std::runtime_error("kevent() failed");
-		}
 		changeList.clear();
 
 		for (int i = 0; i < newEvents; ++i)
 		{
 			currEvent = &eventList[i];
 			if (currEvent->flags & EV_ERROR)
-			{
 				std::cerr << "current event error" << std::endl;
-			}
 			else if (currEvent->filter == EVFILT_READ)
 			{
 				if (currEvent->ident == static_cast<uintptr_t>(socket.getSocket()))
-				{
-					int clientSocket = accept(socket.getSocket(), NULL, NULL);
-					if (clientSocket < 0)
-						std::cerr << "Client accept failed" << std::endl;
-					else
-						std::cout << "Accept client socket " << clientSocket << std::endl;
-					fcntl(clientSocket, F_SETFL, O_NONBLOCK);
-					changeEvents(changeList, clientSocket, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, NULL);
-					changeEvents(changeList, clientSocket, EVFILT_WRITE, EV_ADD | EV_ENABLE, 0, 0, NULL);
-					Client	freshClient(clientSocket, "");
-					clients[clientSocket] = freshClient;
-				}
+					welcomeNewClient();
 				else
-				{
-					std::map<int, Client>::iterator it = clients.find(currEvent->ident);
-					Client currClient = it->second;
-					if (it != clients.end())
-					{
-						char	buf[1024];
-						int n = recv(currEvent->ident, buf, sizeof(buf), 0);
-						if (n <= 0)
-						{
-							if (n < 0)
-								std::cerr << "Error: Client socket has problem\n";
-							disconnectClient(currEvent->ident);
-						}
-						else
-						{
-							buf[n] = '\0';
-							currClient.attachMsg(buf);
-						}
-					}
-				}
+					getClientMsg(currEvent->ident);
 			}
 			else if (currEvent->filter == EVFILT_WRITE)
+				sendResponseMsg();
+		}
+	}
+}
+
+void	Server::welcomeNewClient()
+{
+	int clientSocket = accept(socket.getSocket(), NULL, NULL);
+	if (clientSocket < 0)
+		std::cerr << "Client accept failed" << std::endl;
+	else
+		std::cout << "Accept client socket " << clientSocket << std::endl;
+	fcntl(clientSocket, F_SETFL, O_NONBLOCK);
+	changeEvents(changeList, clientSocket, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, NULL);
+	changeEvents(changeList, clientSocket, EVFILT_WRITE, EV_ADD | EV_ENABLE, 0, 0, NULL);
+	Client	freshClient(clientSocket, "");
+	clients[clientSocket] = freshClient;
+	//rpl_welcome
+}
+
+void	Server::getClientMsg(int currFd)
+{
+	std::map<int, Client>::iterator it = clients.find(currFd);
+	if (it != clients.end())
+	{
+		char	buf[1024];
+		int n = recv(currFd, buf, sizeof(buf), 0);
+		if (n <= 0)
+		{
+			if (n < 0)
+				std::cerr << "Error: Client socket has problem\n";
+			disconnectClient(currFd);
+		}
+		else
+		{
+			buf[n] = '\0';
+			it->second.attachMsg(buf);
+		}
+		//go parsing??
+	}
+}
+
+void	sendResponseMsg(int currFd)
+{
+	std::map<int, Client>::iterator it = clients.find(currFd);
+	Client currClient = it->second;
+	if (it != clients.end())
+	{
+		if (!currClient.getMsg().empty())
+		{
+			int n = send(currFd, currClient.getMsg().c_str(), currClient.getMsg().size(), 0);
+			if (n < 0)
 			{
-				std::map<int, Client>::iterator it = clients.find(currEvent->ident);
-				Client currClient = it->second;
-				if (it != clients.end())
-				{
-					if (!currClient.getMsg().empty())
-					{
-						int n = send(currEvent->ident, currClient.getMsg().c_str(), currClient.getMsg().size(), 0);
-						if (n < 0)
-						{
-							std::cerr << "Error: Write failed\n";
-							disconnectClient(currEvent->ident);
-						}
-						else
-						{
-							currClient.setMsg("");
-						}
-					}
-				}
+				std::cerr << "Error: Write failed\n";
+				disconnectClient(currFd);
 			}
+			else
+				currClient.setMsg("");
 		}
 	}
 }
