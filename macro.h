b509f3331b9a6215f7128253e432f525a68a5ee0
성유진 <syj010820@naver.com>
2024-05-20 17:15:03 +0900

    errmsg 추가

--- macro.h
+++ macro.h
@@ -8,4 +8,11 @@
 # define RPL_CREATED "003"
 # define RPL_MYINFO "004"
 
+# define ERR_NONICKNAMEGIVEN "431"
+# define ERR_NONICKNAMEGIVEN_MSG ":No nickname given"
+# define ERR_ERRONEUSNICKNAME "432"
+# define ERR_ERRONEUSNICKNAME_MSG ":Erroneus nickname"
+# define ERR_NICKNAMEINUSE "433"
+# define ERR_NICKNAMEINUSE_MSG ":Nickname is already in use"
+
 #endif
