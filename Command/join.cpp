913bf67ec0365a5f87e89e789e7c7c0a4270eaa2
vivivim <130360809+vivivim@users.noreply.github.com>
2024-05-27 22:02:57 +0900

    join +irc.local

--- Command/join.cpp
+++ Command/join.cpp
@@ -64,7 +64,7 @@ void	Server::joinChannel(Client& newbie, std::string channelName)
 		msg += IL + " " + RPL_TOPICWHOTIME + " " + newbie.getNick() + " " + channelName + " " + curr.getTopicWho() + " :" + curr.getTopicTime() + "\r\n";
 	}
 	msg += IL + " " + RPL_NAMREPLY + " " + newbie.getNick() + " = " + channelName + " :" + curr.getClientList() + "\r\n";
-	msg += IL + " " RPL_ENDOFNAMES + " " + newbie.getNick() + " " + channelName + " :End of /NAMES list.\r\n\r\n";
+	msg += IL + " " + RPL_ENDOFNAMES + " " + newbie.getNick() + " " + channelName + " :End of /NAMES list.\r\n\r\n";
 	pushResponse(newbie.getFd(), msg);
 	msg = newbie.getNick() + " JOIN :" + channelName + "\r\n\r\n";
 	sendMsgToChannel(channelName, msg);
