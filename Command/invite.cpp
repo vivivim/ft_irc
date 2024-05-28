dedc28e5af2b5bf1c78f584d0dbbf17cbfb75225
성유진 <syj010820@naver.com>
2024-05-28 22:18:44 +0900

    reply msg 매크로 설정정

--- Command/invite.cpp
+++ Command/invite.cpp
@@ -10,10 +10,10 @@ void Server::invite(std::stringstream& ss, Client &currClient)
 	if (!(ss >> invitedUser)) // 무시
 		return ;
 
-	// 피초대자가 존재하지 않음 -> ERR_NOSUCHNICK (irssi 기준)
+	// 피초대자가 존재하지 않음 (irssi 기준)
 	if (getClientFdByNick(invitedUser) == -1)
 	{
-		std::string msg = IL + " 401 " + currClient.getNick() + " " + invitedUser + " :No such nick/channel";
+		std::string msg = IL + " " + ERR_NOSUCHNICK + " " + currClient.getNick() + " " + invitedUser + " " + ERR_NOSUCHNICK_MSG;
 		pushResponse(currClient.getFd(), msg);
 		return;
 	}
@@ -22,19 +22,19 @@ void Server::invite(std::stringstream& ss, Client &currClient)
 	if (!(ss >> channelName)) // 무시
 		return;
 
-	// 채널이 존재하지 않음 -> ERR_NOSUCHCHANNEL
+	// 채널이 존재하지 않음
 	if (channels.find(channelName) == channels.end())
 	{
-		std::string msg = IL + " 403 " + currClient.getNick() + " " + invitedUser + " :No such channel";
+		std::string msg = IL + " " + ERR_NOSUCHCHANNEL + " "+ currClient.getNick() + " " + invitedUser + " " + ERR_NOSUCHCHANNEL_MSG;
 		pushResponse(currClient.getFd(), msg);
 		return;
 	}
 
-	// 초대자(currClient)가 채널의 구성원이 아님 -> ERR_NOTONCHANNEL
+	// 초대자(currClient)가 채널의 구성원이 아님
 	Channel channel = channels.find(channelName)->second;
 	if (!channel.IsUserInChannel(currClient.getNick()))
 	{
-		std::string msg = IL + " 442 " + currClient.getNick() + " " + channelName + " :You're not on that channel";
+		std::string msg = IL + " " + ERR_NOTONCHANNEL + " " + currClient.getNick() + " " + channelName + " " + ERR_NOTONCHANNEL_MSG;
 		pushResponse(currClient.getFd(), msg);
 		return;
 	}
@@ -50,7 +50,7 @@ void Server::invite(std::stringstream& ss, Client &currClient)
 	// 피초대자가 이미 채널에 존재 -> ERR_USERONCHANNEL
 	if (channel.IsUserInChannel(invitedUser))
 	{
-		std::string msg = IL + " 443 " + currClient.getNick() + " " + invitedUser + " " + channelName + " :is already on channel";
+		std::string msg = IL + " " + ERR_USERONCHANNEL + " " + currClient.getNick() + " " + invitedUser + " " + channelName + " " + ERR_USERONCHANNEL_MSG;
 		pushResponse(currClient.getFd(), msg);
 		return;
 	}
@@ -58,7 +58,7 @@ void Server::invite(std::stringstream& ss, Client &currClient)
 	// 초대 성공
 	channel.addInvited(invitedUser);
 	// 초대자가 받는 응답 메시지 -> :irc.local 341 user1 user3 :#b 
-	std::string msg = IL + " 341 " + currClient.getNick() + " " + invitedUser + " :" + channelName;
+	std::string msg = IL + " " + RPL_INVITING + " " + currClient.getNick() + " " + invitedUser + " :" + channelName;
 	pushResponse(currClient.getFd(), msg);
 	// 피초대자가 받는 응답 메시지 -> user1!root@127.0.0.1 INVITE user3 :#b
 	msg = ":" + currClient.getNick() + ADR + " INVITE " + invitedUser + " :" + channelName;
--- Utils/Macro.h
+++ Utils/Macro.h
@@ -7,6 +7,7 @@ const std::string SERVER_NAME = "ircserv";
 
 const std::string IL = ":irc.local";
 const std::string ADR = "!root@127.0.0.1";
+
 // WELCOME
 const std::string RPL_WELCOME = "001";
 const std::string RPL_WELCOME_MSG = "Welcome to the Internet Relay Network";
@@ -20,12 +21,24 @@ const std::string RPL_TOPICWHOTIME = "333";
 const std::string RPL_NAMREPLY = "353";
 const std::string RPL_ENDOFNAMES = "366";
 
+// INVITE_REPLY
+const std::string RPL_INVITING = "341";
+
 // ERR
+const std::string ERR_NOSUCHNICK = "401";
+const std::string ERR_NOSUCHNICK_MSG = ":No such nick/channel";
+const std::string ERR_NOSUCHCHANNEL = "403";
+const std::string ERR_NOSUCHCHANNEL_MSG = ":No such channel";
 const std::string ERR_NONICKNAMEGIVEN = "431";
 const std::string ERR_NONICKNAMEGIVEN_MSG = ":No nickname given";
 const std::string ERR_ERRONEUSNICKNAME = "432";
 const std::string ERR_ERRONEUSNICKNAME_MSG = ":Erroneus nickname";
 const std::string ERR_NICKNAMEINUSE = "433";
 const std::string ERR_NICKNAMEINUSE_MSG = ":Nickname is already in use";
+const std::string ERR_NOTONCHANNEL = "442";
+const std::string ERR_NOTONCHANNEL_MSG = ":You're not on that channel";
+const std::string ERR_USERONCHANNEL = "482";
+const std::string ERR_USERONCHANNEL_MSG = ":is already on channel";
+
 
 #endif
