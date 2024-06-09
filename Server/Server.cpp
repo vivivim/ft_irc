af2a920cc5e4520f09fc4f57cb3e021cd0fd7744
성유진 <syj010820@naver.com>
2024-06-09 17:51:52 +0900

    join 채널명과 키 여러개 받도록 수정

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -127,20 +127,6 @@ void	Server::getClientMsg(int currFd)
 	}
 }
 
-std::vector<std::string> split(const std::string& str, const std::string& delimiter) {
-	std::vector<std::string> tokens;
-	size_t start = 0;
-	size_t end = str.find(delimiter);
-
-	while (end != std::string::npos) {
-		tokens.push_back(str.substr(start, end - start));
-		start = end + delimiter.length();
-		end = str.find(delimiter, start);
-	}
-	tokens.push_back(str.substr(start));
-
-	return tokens;
-}
 
 void	Server::letsGoParsing(Client& currClient)
 {
@@ -287,4 +273,19 @@ int	Server::getClientFdByNick(std::string nick)
 			return it->second.getFd();
 	}
 	return -1;
+}
+
+std::vector<std::string> split(const std::string& str, const std::string& delimiter)
+{
+	std::vector<std::string> tokens;
+	size_t start = 0;
+	size_t end = str.find(delimiter);
+
+	while (end != std::string::npos) {
+		tokens.push_back(str.substr(start, end - start));
+		start = end + delimiter.length();
+		end = str.find(delimiter, start);
+	}
+	tokens.push_back(str.substr(start));
+	return tokens;
 }
